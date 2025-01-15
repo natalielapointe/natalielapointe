7679d5d87649ca1fedb98311e416f22cb9e7d6e8
Natalie LaPointe <hello@natalielapointe.com>
2025-01-14 19:04:54 -0600

    Remove misused headers, fix FOUC

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,45 +8,45 @@ import folderIconPink from './images/folderIconPink.svg';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="background shimmer">
+      <body className="background shimmer" style={{ display: 'none' }}>
         <div className="background-pixel-grid gird-shift">
           <div className="container-wrapper">
             <div className="pixel-corners--wrapper">
               <div className="overflow">
                 <div className='file-name-container shimmer'>
-                  <h2>portfolio.html</h2>
+                  <span id="fileName" aria-hidden="true">portfolio.html</span>
                   <span className="close-button"></span>
                 </div>
                 {children}
               </div>
             </div>
           </div>
-          <div className="nav-wrapper">
+          <nav className="nav-wrapper">
             <Link href="/">
               <div className='nav-item-wrapper'>
                 <img src={folderIcon} />
-                <h3>Home</h3>
+                <span>Home</span>
               </div>
             </Link>
             <Link href="/about-me">
               <div className='nav-item-wrapper'>
                 <img src={folderIconOrange} />
-                <h3>About <br/> me</h3>
+                <span>About <br/> me</span>
               </div>
             </Link>
             <Link href="/resume">
               <div className='nav-item-wrapper'>
                 <img src={folderIconPurple} />
-                <h3>Resume</h3>
+                <span>Resume</span>
               </div>
             </Link>
             <Link href="/contact">
               <div className='nav-item-wrapper'>
                 <img src={folderIconPink} />
-                <h3>Contact</h3>
+                <span>Contact</span>
               </div>
             </Link>
-          </div>
+          </nav>
         </div>
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -6,20 +6,20 @@ export default function Home() {
   return (
     <div className='background-white flex-column'>
       <h1 className="gradient-text">Natalie <br/> LaPointe</h1>
-      <div className="gradient-text padding-bottom">
-        <span className="title-wrapper">
-          <img src={laptopIcon} />
-          <h2>Front End Developer</h2>
-        </span>
-        <span className="title-wrapper">
-          <img src={a11yIcon} />
-          <h2>Accessibility Expert</h2>
-        </span>
-        <span className="title-wrapper">
-          <img src={graphicDesignerIcon} />
-          <h2>Graphic Designer</h2>
-        </span>
-      </div>
+      <ul className="gradient-text padding-bottom home-list">
+        <li className="title-wrapper">
+          <img src={laptopIcon} alt="" />
+          <span>Front End Developer</span>
+        </li>
+        <li className="title-wrapper">
+          <img src={a11yIcon} alt=""/>
+          <span>Accessibility Expert</span>
+        </li>
+        <li className="title-wrapper">
+          <img src={graphicDesignerIcon} alt=""/>
+          <span>Graphic Designer</span>
+        </li>
+      </ul>
     </div>
   );  
 };
