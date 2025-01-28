7eebf3a28c00c6c781063325d77e18e45983f857
Natalie LaPointe <hello@natalielapointe.com>
2025-01-28 08:43:59 -0600

    Add pixel star graphics

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
 import laptopIcon from './images/laptopIcon.svg';
 import a11yIcon from './images/a11yIcon.svg';
 import graphicDesignerIcon from './images/graphicDesignerIcon.svg';
+import pixelStar from './images/pixelStar.svg';
 
 export default function Home() {
   return (
     <section id="home" className='background-white flex-column'>
-      <h1 tabIndex="0" className="gradient-text">Natalie <br/> LaPointe</h1>
+      <div className="header-wrapper">
+        <img src={pixelStar} className="pixel-star-1" alt="" />
+        <h1 tabIndex="0" className="gradient-text">Natalie <br/> LaPointe</h1>
+        <img src={pixelStar} className="pixel-star-2" alt="" />
+      </div>
       <ul className="gradient-text padding-bottom home-list">
         <li className="title-wrapper">
           <img src={laptopIcon} alt="" />
